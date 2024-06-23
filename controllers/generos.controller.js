50465f04370cc153104ea497f1295ad2b20f64b8
colococo <jorge_mattii@hotmail.com>
2024-06-23 02:18:11 -0300

    Método PUT en géneros

--- controllers/generos.controller.js
+++ controllers/generos.controller.js
@@ -29,9 +29,31 @@ const generos = (req,res) => {
     });
 };
 
+const updateGenero = (req, res) => {
+    const { id } = req.params;
+    const { nombre_genero } = req.body;
+    // consulta update(PUT), importante que este el WHERE id
+    // nombre_genero = ?: quiere decir cuando sea igual a algo (algo: viene del fron ?: por los hackers)
+    const sql = "UPDATE generos SET nombre_genero = ? WHERE id = ?";
+    db.query(sql, [nombre_genero, id], (error, result) => {
+        if (error) {
+            return res.status(500).json({ error: "Intente mas tarde" });
+        }
+        // si el result no tuvo ningun cambio(affectedRows: no cambios en la fila de la tabla)
+        if (result.affectedRows == 0) {
+            return res.status(404).json({ error: "No existe el género" })
+        }
+
+        // si se produce el update devuelvo los cambios consola postman
+        const update = { ...req.body, ...req.params };
+
+        res.json(update);
+    });
+};
 
 
 module.exports = {
     NewGenero,
     generos,
+    updateGenero,
 };
--- routes/generos.router.js
+++ routes/generos.router.js
@@ -3,7 +3,8 @@ const router = express.Router();
 
 const controller = require('../controllers/generos.controller');
 
-router.get('/', controller.generos); // GET: todos los generos
-router.post('/', controller.NewGenero); // POST: agregando un genero
+router.get('/', controller.generos); // GET: todos los géneros
+router.post('/', controller.NewGenero); // POST: agregando un género
+router.put('/:id', controller.updateGenero); // PUT: modificando un género
 
 module.exports = router;
