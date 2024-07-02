303f3904ee813cbfeeead8cd41c468c350d35472
devlucasarielrotta <lucas.ariel.rotta@gmail.com>
2024-07-01 23:01:21 -0300

    added public index

--- dist/index.js
+++ dist/index.js
@@ -3,18 +3,21 @@
 var express = require('express');
 var app = express();
 var path = require('path');
-app.use(express.json());
-var actoresRoutes = require("./routes");
-app.use(actoresRoutes);
-// app.use('api/generos', require("./routes/generos.router"));
-// app.use('/comentarios', require("./routes/comentarios.router"));
-
-app.use('/', function (req, res) {
-  res.send('Proyecto integrador Grupo 23');
-});
-
-// comentarios nuevos
 var PORT = process.env.PORT || 3000;
+var pelicuasRoutes = require("./routes");
+app.use(express.json());
+
+// Ruta estática y pública 
+app.use(express["static"]('public'));
+
+// Rutas de la aplicación 
+app.use(pelicuasRoutes);
+
+// Ruta comodín, en caso de poner alguna ruta invalida cae aca.
+app.get('*', function (req, res) {
+  var indexPath = path.join(__dirname + "../../public/index.html");
+  res.sendFile(indexPath);
+});
 app.listen(PORT, function () {
   console.log("http://localhost:".concat(PORT));
 });
--- src/index.js
+++ src/index.js
@@ -1,18 +1,23 @@
 const express = require('express');
 const app = express();
-
 const path = require('path');
 
-app.use(express.json());
+const PORT = process.env.PORT || 3000;
+
 const pelicuasRoutes = require("./routes")
 
+app.use(express.json());
+
+// Ruta estática y pública 
+app.use( express.static('public') );
+
+// Rutas de la aplicación 
 app.use(pelicuasRoutes);
 
-app.use('/', (req, res)=>{
-    res.send('Proyecto integrador Grupo 23');
-});
-
-// comentarios nuevos
-const PORT = process.env.PORT || 3000;
+// Ruta comodín, en caso de poner alguna ruta invalida cae aca.
+app.get('*',(req,res) => {
+    const indexPath = path.join( __dirname + `../../public/index.html` );
+    res.sendFile(indexPath);
+})
 
 app.listen(PORT, ()=>{console.log(`http://localhost:${PORT}`)});
